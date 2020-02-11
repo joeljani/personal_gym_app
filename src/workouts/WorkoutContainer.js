1807d0826dc60fcb3fd40d20f5d21ad7284d9dad
Joel Jani <joel.luca.jani@gmail.com>
2020-02-11 15:40:50 +0100

    Add logic for fetching workouts

--- src/workouts/WorkoutContainer.js
+++ src/workouts/WorkoutContainer.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
 
 const WorkoutContainer = ({serverUrl}) => {
+
+    const workouts = useSelector(state => state.workouts);
+    const dispatch = useDispatch();
+
+
+    const fetchWorkouts = () => {
+        dispatch(
+            async dispatch => {
+                const res = await fetch(serverUrl + "/workouts");
+                const data = await res.json();
+                dispatch({type: "WORKOUTS_FETCHED", workouts: data});
+            });
+    }
+
+    useEffect(fetchWorkouts, [serverUrl]);
+
+
     return (
         <div>
-            <h1>{serverUrl}</h1>
+            <ul>
+                {workouts.map(workout => <li key={workout.id}>{workout.name}</li>)}
+            </ul>
         </div>
     )
 }
