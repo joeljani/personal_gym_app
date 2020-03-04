79bf1e1f98a26659a8d9692be982608c4efe007c
Joel Jani <joel.luca.jani@gmail.com>
2020-03-04 09:54:31 +0100

    Update workoutController

--- web/dispatcher.js
+++ web/dispatcher.js
@@ -5,6 +5,9 @@ const exerciseController = require('./exerciseController')
 
 dispatcher.route('/workouts').get(workoutController.findAll)
 dispatcher.route('/workouts').post(workoutController.create)
+dispatcher.route('/workouts/:id').delete(workoutController.delete)
+dispatcher.route('/workouts/:id').put(workoutController.update)
+
 
 
 module.exports = dispatcher;
--- web/workoutController.js
+++ web/workoutController.js
@@ -1,30 +1,28 @@
 const log4js = require('log4js')
-const Workout = require('../domain/workout')
+const WorkoutModel = require('../domain/workout')
 const logger = log4js.getLogger('controller')
 
 
 exports.findAll = (req, res) => {
-    Workout.find((err, workouts) => {
-        if (err) {
-            return res.status(400).send('database error')
-        }
+    WorkoutModel.find((err, workouts) => {
+        if (err) return res.status(400).send('database error')
         logger.debug(`Found ${workouts.length} workouts`)
         res.status(200).json(workouts)
     })
 }
 
 exports.create = (req, res) => {
-    // Create a new instance of the Questionnaire model
-    Workout.find((err, workouts) => {
+    WorkoutModel.find((err, workouts) => {
         const existingWorkout = workouts.find(w => w.date === req.body.date)
-        if(existingWorkout === undefined) {
-            let workout = new Workout()
+        if (existingWorkout === undefined) {
+            let workout = new WorkoutModel()
             workout.date = req.body.date
             workout.name = req.body.name
             workout.notes = req.body.notes
             workout.exercises = req.body.exercises
 
-            // Save the questionnaire and check for errors
+            logger.debug(workout)
+
             workout.save((err, workoutCreated) => {
                 if (err) {
                     logger.error(`Could not create new Workout`)
@@ -38,5 +36,37 @@ exports.create = (req, res) => {
             logger.error(`Workout already exists at that date`)
             res.status(412).send('database error')
         }
+    })
+}
+
+exports.delete = (req, res) => {
+    WorkoutModel.deleteOne({_id: req.params.id}, err => {
+        if (err) {
+            logger.error(`Could not delete Workout with id "${req.params.id}"`)
+            return res.status(404).send('database error')
+        }
+        logger.debug(`Successfully deleted Workout with id "${req.params.id}"`)
+        res.status(200).send()
+    })
+}
+
+exports.update = (req, res) => {
+    WorkoutModel.findById(req.params.id, (err, workout) => {
+        if (err) {
+            logger.error(`Could not update workout with id "${req.params.id}"`)
+            return res.status(404).send('database error')
+        }
+        workout.date = req.body.date
+        workout.name = req.body.name
+        workout.notes = req.body.notes
+        workout.exercises = req.body.exercises
+
+        workout.save(err => {
+            if (err) {
+                return res.status(404).send('database error')
+            }
+            logger.debug(`Successfully updated workout with id "${workout.id}"`)
+            res.status(200).json(workout)
+        })
     })
 }
