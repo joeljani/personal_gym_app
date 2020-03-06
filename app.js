f9c695987ebb7dbbe206530ce2e7c1a72981e708
Joel Jani <joel.luca.jani@gmail.com>
2020-03-06 18:17:33 +0100

    Add Dockerfile

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict"
+
+const log4js = require('log4js')
+const dotenv = require('dotenv-extended')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const routes = require('./web/dispatcher')
+require('dotenv').config()
+
+// Read the properties from file '.env' and '.env.defaults'
+dotenv.load({ silent: true })
+
+log4js.configure('log4js.json')
+const logger = log4js.getLogger('app')
+
+mongoose.Promise = global.Promise
+
+const url = 'mongodb://' + process.env.MONGO_HOST_LOCAL + '/' + process.env.MONGO_DATABASE_LOCAL
+logger.debug(`Database URL used '%s'`, url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+
+const app = express()
+
+app.use(bodyParser.json())
+
+// Enable CORS (for all requests)
+app.use(cors())
+
+// Configure the dispatcher with all its controllers
+app.use('', routes)
+
+// Read PORT from the configuration, default to 9090
+const PORT = process.env.PORT || 8080
+
+// Start the App as HTTP server
+app.listen(PORT, () => console.log(`Running on port ${PORT}!`))
+
+logger.info(`Server started on port ${PORT}`)
+
+module.exports = app
