89d80df0a1d7fdaf7fae74af50816c4c1c238cb2
Joel Jani <joel.luca.jani@gmail.com>
2020-04-12 10:12:51 +0200

    Fix test setup

--- test/testHelper.js
+++ test/testHelper.js
@@ -1,16 +1,27 @@
 const assert = require('assert');
-const WorkoutModel = require('../domain/workout')
-let request = require('supertest')
-const app = require('../app')
+const WorkoutModel = require('../domain/workout');
+let request = require('supertest');
 const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('../web/dispatcher');
 
+//TODO: Refactor -> needs proper structure
+const testApp = express();
+
+const testPort = 8081;
+testApp.listen(testPort, () => console.log(`Running on port ${testPort}!`)); // Start app as HTTP server
+testApp.use(bodyParser.json());
+testApp.use(cors());
+testApp.use('', routes); // Configure the dispatcher with all its controllers
 
 /**
  * Relocated methods => DRY principle
  */
-
 const initDB = () => {
-    const mongodbUrl = "mongodb://localhost:27017/gymPlanner"
+    const mongodbUrl = "mongodb://localhost:27017/gymPlanner_testDB";
+    mongoose.connect(mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
     describe('Check and prepare DB', function () {
         it('should be connected to DB', function () {
             assert.equal(mongoose.connection.client.s.url, mongodbUrl)
@@ -21,24 +32,24 @@ const initDB = () => {
             })
         });
     })
-}
+};
 
 
 const getOneWorkoutFromDB = async () => {
-    const doc = await WorkoutModel.findOne({})
-    const workoutObject = JSON.parse(JSON.stringify(doc)) //Workaround because mongoose query returns a documentArray
+    const doc = await WorkoutModel.findOne({});
+    const workoutObject = JSON.parse(JSON.stringify(doc)); //Workaround because mongoose query returns a documentArray
     return workoutObject
-}
+};
 
 const deleteMongooseGeneratedValues = obj => {
-    delete obj._id
-    delete obj.__v
-    if (obj.workout !== undefined) delete obj.workout
+    delete obj._id;
+    delete obj.__v;
+    if (obj.workout !== undefined) delete obj.workout;
     if (obj.exercises !== undefined) {
         obj.exercises.forEach((e) => deleteMongooseGeneratedValues(e))
     }
     return obj
-}
+};
 
 const createWorkout = () => {
     const exercise = {
@@ -48,47 +59,47 @@ const createWorkout = () => {
         sets: 5,
         kg: 70,
         goal: '3x5'
-    }
+    };
 
     const workout = {
         date: '2020-02-02',
         name: 'Push',
-        notes: 'random notes',
+        description: 'random notes',
         exercises: [
             exercise
         ]
     };
 
     it('should create a new workout', function () {
-        return request(app).post('/workouts')
+        return request(testApp).post('/workouts')
             .send(workout)
             .then(async res => {
-                assert.equal(res.statusCode, 200)
-                const responseWorkout = deleteMongooseGeneratedValues({...res.body})
+                assert.equal(res.statusCode, 200);
+                const responseWorkout = deleteMongooseGeneratedValues({...res.body});
                 assert.deepEqual(responseWorkout, workout)
             })
     });
 
     it('should have 1 workout in the database', async function () {
-        return request(app).get('/workouts')
+        return request(testApp).get('/workouts')
             .then(res => assert.equal(res.body.length, 1))
     })
-}
+};
 
 const updateWorkout = editedWorkout => {
-    return request(app).put('/workouts/' + editedWorkout._id)
+    return request(testApp).put('/workouts/' + editedWorkout._id)
         .send(editedWorkout)
         .then(async res => {
-            assert.equal(res.statusCode, 200)
-            const responseWorkout = deleteMongooseGeneratedValues({...res.body})
-            const editedWorkout_ = deleteMongooseGeneratedValues({...editedWorkout})
+            assert.equal(res.statusCode, 200);
+            const responseWorkout = deleteMongooseGeneratedValues({...res.body});
+            const editedWorkout_ = deleteMongooseGeneratedValues({...editedWorkout});
             assert.deepEqual(responseWorkout, editedWorkout_)
         })
-}
+};
 
 const deleteWorkout = workout => {
-    return request(app).delete('/workouts/' + workout._id)
-}
+    return request(testApp).delete('/workouts/' + workout._id)
+};
 
 
 module.exports = {
@@ -96,5 +107,6 @@ module.exports = {
     updateWorkout: updateWorkout,
     getOneWorkoutFromDB, getOneWorkoutFromDB,
     initDB, initDB,
-    deleteWorkout, deleteWorkout
-}
+    deleteWorkout, deleteWorkout,
+    testApp
+};
--- test/workoutControllerTest.js
+++ test/workoutControllerTest.js
@@ -1,7 +1,8 @@
-let request = require('supertest')
-const app = require('../app')
+let request = require('supertest');
 const assert = require('assert');
-const testHelper = require('../test/testHelper')
+const testHelper = require('../test/testHelper');
+
+const app = testHelper.testApp;
 
 
 /**
@@ -11,7 +12,7 @@ const testHelper = require('../test/testHelper')
 describe('#WorkoutController basic CRUD Tests', function () {
 
     //TODO: bug fix before
-    before(async () => await testHelper.initDB())
+    before(async () => await testHelper.initDB());
 
     context('#GET /workouts', function () {
         it('should respond with array with length 0', function () {
@@ -26,45 +27,45 @@ describe('#WorkoutController basic CRUD Tests', function () {
 
     context('#PUT /workouts/:id', function () {
         it('should save the edited workout correctly', async function () {
-            const workout = await testHelper.getOneWorkoutFromDB()
-            const editedWorkout = {...workout, name: "Pull"}
+            const workout = await testHelper.getOneWorkoutFromDB();
+            const editedWorkout = {...workout, name: "Pull"};
             return testHelper.updateWorkout(editedWorkout)
         })
-    })
+    });
 
     context('#DELETE /workouts/:id', function () {
         it('should respond with array with length 0', async function () {
-            const workout = await testHelper.getOneWorkoutFromDB()
+            const workout = await testHelper.getOneWorkoutFromDB();
             return request(app).delete('/workouts/' + workout._id)
                 .then(res => {
-                    assert.equal(res.statusCode, 200)
-                    assert.equal(Object.keys(res.body).length, 0)
+                    assert.equal(res.statusCode, 200);
+                    assert.equal(Object.keys(res.body).length, 0);
                     assert.equal(res.text, 'deleted successfully')
                 })
         });
     })
 
-})
+});
 
 describe('#WorkoutController and exerciseService integration tests', function () {
 
     before(async () => {
         await testHelper.initDB()
-    })
+    });
 
-    testHelper.createWorkout()
+    testHelper.createWorkout();
 
     context('#PUT /workouts/:id', function () {
         it('should save the edited exercise correctly', async function () {
-            const workout = await testHelper.getOneWorkoutFromDB()
-            const exercise = workout.exercises[0]
-            const editedExercise = {...exercise, name: "Deadlift"}
-            const editedWorkout = {...workout, exercises: [editedExercise]}
+            const workout = await testHelper.getOneWorkoutFromDB();
+            const exercise = workout.exercises[0];
+            const editedExercise = {...exercise, name: "Deadlift"};
+            const editedWorkout = {...workout, exercises: [editedExercise]};
             return testHelper.updateWorkout(editedWorkout)
-        })
+        });
 
         it('should save the new added exercise correctly', async function () {
-            const workout = await testHelper.getOneWorkoutFromDB()
+            const workout = await testHelper.getOneWorkoutFromDB();
             const newExercise = {
                 name: 'Pull up',
                 achieved: true,
@@ -72,10 +73,10 @@ describe('#WorkoutController and exerciseService integration tests', function ()
                 sets: 4,
                 kg: 0,
                 goal: '10x4'
-            }
-            const editedWorkout = {...workout, exercises: workout.exercises.concat(newExercise)}
+            };
+            const editedWorkout = {...workout, exercises: workout.exercises.concat(newExercise)};
             return testHelper.updateWorkout(editedWorkout)
-        })
+        });
 
         it('should return an array of length 2', async function () {
             return request(app).get('/exercises')
@@ -83,7 +84,7 @@ describe('#WorkoutController and exerciseService integration tests', function ()
         })
 
     })
-})
+});
 
 
 
